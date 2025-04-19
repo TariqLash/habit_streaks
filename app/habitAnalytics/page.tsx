e1ca100ecd974903166ba9b1ff2632a56d56d124
Tariq Lashley <tariqlash@gmail.com>
2025-04-19 15:16:45 -0600

    cleaned up task page a bit, now need list

--- app/habitAnalytics/page.tsx
+++ app/habitAnalytics/page.tsx
@@ -1,47 +1,78 @@
+import { Button } from '@/components/ui/button';
 import React from 'react'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
 
 const habitAnalytics = () => {
 
   const utcDate = new Date(); // UTC time
-const now = new Date(utcDate); // Automatically converts to local time
-const dates = [];
+  const now = new Date(utcDate); // Automatically converts to local time
+  const dates = [];
 
-// Get 6 days before today
-for (let i = 6; i > 0; i--) {
-  const prevDate = new Date(now);
-  prevDate.setDate(now.getDate() - i);
-  dates.push(prevDate);
-}
+  // Get 6 days before today
+  for (let i = 6; i > 0; i--) {
+    const prevDate = new Date(now);
+    prevDate.setDate(now.getDate() - i);
+    dates.push(prevDate);
+  }
 
-// Add today
-dates.push(new Date(now));
+  // Add today
+  dates.push(new Date(now));
 
-const formattedDates = dates.map(date => date.toDateString());
+  const formattedDates = dates.map(date => date.toDateString());
 
-const dateNums = [];
-const dateDays = [];
+  const dateNums = [];
+  const dateDays = [];
 
-for(let i=0; i<formattedDates.length; i++ ){
-  const parts = formattedDates[i].split(' ');
-  dateNums.push(parts[2])
-  dateDays.push(parts[0])
-}
+  for (let i = 0; i < formattedDates.length; i++) {
+    const parts = formattedDates[i].split(' ');
+    dateNums.push(parts[2])
+    dateDays.push(parts[0])
+  }
 
-console.log(dateDays)
-console.log(dateNums)
+  console.log(dateDays)
+  console.log(dateNums)
 
-// console.log(formattedDates);
+  // console.log(formattedDates);
 
   return (
-    <div className='bg-neutral-300'>
-      <div className='bg-red-400 flex justify-around px-16'>
+    <div className='bg-neutral-300 m-8 h-screen'>
+      <div className='flex justify-between mb-8'>
         {dateNums.map((num, index) => (
-          <div className='flex flex-col bg-blue-100 h-16 w-16 justify-center items-center'>
-            <span className='text-2xl'>{num}</span> 
+          <Button key={num} className='flex flex-col size-24 justify-center items-center'>
+            <span className='text-2xl'>{num}</span>
             <span className="text-xs">{dateDays[index]}</span>
-          </div>
+          </Button>
         ))}
       </div>
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
     </div>
   )
 }
